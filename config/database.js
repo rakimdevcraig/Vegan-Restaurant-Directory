fee880636c155a35443c9e98210aed0217a3479f
rakimdevcraig <rakimdevcraig@gmail.com>
2018-07-22 05:29:30 -0400

    rendering info from database

---
+++ config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://admin:password1@ds147011.mlab.com:47011/vegproject' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+};
